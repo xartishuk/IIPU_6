1d7f34cf77dac1bafa6865ce95d892454eb16876
xartishuk <xartishuk@gmail.com>
2018-01-29 00:08:16 +0300

    exception handling

--- WirelessManager/Connector/WiFiConnector.cs
+++ WirelessManager/Connector/WiFiConnector.cs
@@ -1,70 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WirelessManager.Network;
 using NativeWifi;
-using SimpleWifi;
 
-namespace WirelessManager.Connector
+namespace WirelessManager.Scanner
 {
-    sealed class WiFiConnector
+    sealed class WiFiScanner
     {
-        private static WiFiConnector _instance;
-        public static WiFiConnector GetInstance() { return _instance ?? (_instance = new WiFiConnector()); }
-        /*
-        public void Connect(string name, string password)
+        private static WiFiScanner _instance;
+        private WiFiScanner() { }
+        public static WiFiScanner GetInstance()
         {
+            return _instance ?? (_instance = new WiFiScanner());
+        }
+
+        public List<WiFiNetwork> Networks { get; private set; }
+        public List<WiFiNetwork> UpdateNetworks()
+        {
+            var wifiList = new List<WiFiNetwork>();
             var client = new WlanClient();
             foreach (var wlanIface in client.Interfaces)
             {
                 var wlanBssEntries = wlanIface.GetAvailableNetworkList(0);
+                var macIndex = 0;
                 foreach (var network in wlanBssEntries)
                 {
-                    var profileName = Encoding.ASCII.GetString(network.dot11Ssid.SSID);
-                    if (profileName == name)
+                    var wifi = new WiFiNetwork();
+                    wifi.Name = Encoding.ASCII.GetString(network.dot11Ssid.SSID);
+                    wifi.ConnectionQuality = network.wlanSignalQuality.ToString() + "%";
+                    wifi.AuthenticationType = network.dot11DefaultAuthAlgorithm.ToString();
+                    if (wifiList.Contains(wifi)) { continue; }
+                    try
                     {
-                        var strTemplate = "";
-                        var authentication = "";
-                        switch ((int)network.dot11DefaultAuthAlgorithm)
-                        {
-                            case 1: // Open
-                                break;
-                            case 3: // WEP
-                                break;
-                            case 4: // WPA_PSK
-                                strTemplate = Properties.Resources.WPAPSK;
-                                authentication = "WPAPSK";
-                                var encryption = network.dot11DefaultCipherAlgorithm.ToString().Trim((char)0);
-                                var profileXml = string.Format(strTemplate, profileName, authentication, encryption, password);
-                                wlanIface.SetProfile(Wlan.WlanProfileFlags.AllUser, profileXml, true);
-                                wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
-                                break;
-
-                            default:
-                                break;
-                        }
-                    }
+                        wifi.MAC = wlanIface.GetNetworkBssList()[macIndex].dot11Bssid.Select(b => b.ToString("x2")).Aggregate((cur, next) => cur + ':' + next);//.Trim((char)0);
+                    } catch { break; }
+                    wifi.Connected = network.flags.HasFlag(Wlan.WlanAvailableNetworkFlags.Connected);
+                    wifiList.Add(wifi);
+                    macIndex++;
                 }
             }
-        }
-        */
-
-
-        public bool Connect(string name, string password)
-        {
-            var wifi = new Wifi();
-            var selectedAP = wifi.GetAccessPoints().First(p => p.Name == name.Trim((char)0));
-            if (selectedAP != null)
-            {
-                var authRequest = new AuthRequest(selectedAP);
-                if (authRequest.IsPasswordRequired)
-                {
-                    authRequest.Password = password;
-                }
-                return selectedAP.Connect(authRequest);
-            }
-            return false;
+            return Networks = wifiList;
         }
     }
 }
